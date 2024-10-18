73462dae7525ae16aad48ac8cfeada10776831eb
Krittika-Tiwari <krittikatiwari122004@gmail.com>
2024-10-18 13:26:52 +0530

    creating auth screen

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUPCard } from "@/features/auth/components/sign-up-card";
+import React from "react";
+
+const SignUpPage = () => {
+  return <SignUPCard />;
+};
+
+export default SignUpPage;
