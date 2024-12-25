4b071bc75365f8c6f3ce1b4fdd7103f058ac3cfc
Krittika-Tiwari <krittikatiwari122004@outlook.com>
2024-12-25 22:47:29 +0530

    session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-
+"use client";
 import { Button } from "@/components/ui/button";
-import { createAdminClient } from "@/lib/appwrite";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
-  console.log(createAdminClient);
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div className="">
-      <Button disabled size={"lg"} variant={"primary"}>
-        Heelo
-      </Button>
-      <Button variant={"destructive"}>destructive</Button>
-      <Button variant={"outline"}>outline</Button>
-      <Button variant={"secondary"}>secondary</Button>
-      <Button variant={"ghost"}>ghost</Button>
-      <Button variant={"muted"}>muted</Button>
-      <Button variant={"teritrary"}>teritrary</Button>
+      Only visible to authenticated users
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
